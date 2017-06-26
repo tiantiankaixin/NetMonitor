85fe62f0611049f0d9cd752f0f4fac9e9ea4a6eb
maliang <295011445@qq.com>
2017-06-26 16:57:14 +0800

    …

---
+++ NetMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetMonitor
+//
+//  Created by maliang on 2017/6/25.
+//  Copyright © 2017年 maliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetMonitor
+//
+//  Created by maliang on 2017/6/25.
+//  Copyright © 2017年 maliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
